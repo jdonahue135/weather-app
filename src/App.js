b470ff576840d3afc602d9c7c66588c1620db648
Jake Donahue <jdonahue135@gmail.com>
2020-05-11 10:55:01 -0400

    format code with Prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 import "whatwg-fetch";
-import './App.css';
+import "./App.css";
 
-import Display from './Display'
-import Image from './Image'
-import CityForm from './CityForm'
+import Display from "./Display";
+import Image from "./Image";
+import CityForm from "./CityForm";
 
-import { getApiUrl, formatTemp } from './helpers'
+import { getApiUrl, formatTemp } from "./helpers";
 
 class App extends React.Component {
   constructor(props) {
@@ -18,18 +18,17 @@ class App extends React.Component {
     this.state = {
       weather: null,
       temp: null,
-      city: 'Boston',
-      format: 'f',
+      city: "Boston",
+      format: "f",
       gif: null,
-      formInput: ''
-    }
+      formInput: "",
+    };
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.city !== this.state.city) {
       this.fetchWeather();
       console.log(this.state);
-      
     }
     if (prevState.weather !== this.state.weather) {
       this.fetchImage();
@@ -37,52 +36,58 @@ class App extends React.Component {
   }
 
   fetchWeather() {
-    fetch(getApiUrl(this.state.city), {mode: 'cors'})
-      .then(resp => {
+    fetch(getApiUrl(this.state.city), { mode: "cors" })
+      .then((resp) => {
         if (!resp.ok) {
           throw Error(resp.statusText);
         }
-        return resp.json()
+        return resp.json();
       })
-      .then(resp => {
+      .then((resp) => {
         const weather = resp.weather[0].main;
-        
+
         const temp = formatTemp(resp.main.temp, this.state.format);
 
         this.setState({
           temp,
-          weather
-        })
-      }).catch(error => console.log(error))
+          weather,
+        });
+      })
+      .catch((error) => console.log(error));
   }
 
   fetchImage() {
     const weather = this.state.weather;
-    let url = 'https://api.giphy.com/v1/gifs/translate?api_key=wqsy7csWQKiCwyrmlWrbs4KEj1AqqyMc&s=' + weather;
-    fetch(url, { mode: 'cors'})
-        .then(response => response.json())
-        .then(response => {
-            this.setState({
-              gif: response.data.images.original.url
-            }, console.log('state set in fetchImage'))
-        })
+    let url =
+      "https://api.giphy.com/v1/gifs/translate?api_key=wqsy7csWQKiCwyrmlWrbs4KEj1AqqyMc&s=" +
+      weather;
+    fetch(url, { mode: "cors" })
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState(
+          {
+            gif: response.data.images.original.url,
+          },
+          console.log("state set in fetchImage")
+        );
+      });
   }
 
   handleInput(e) {
-    let formInput = e.target.value
+    let formInput = e.target.value;
     this.setState({
-        formInput: formInput
-      });
-  } 
+      formInput: formInput,
+    });
+  }
 
   toggleFormat() {
-    if (this.state.format === 'f') {
+    if (this.state.format === "f") {
       this.setState({
-        format: 'c'
+        format: "c",
       });
     } else {
       this.setState({
-        format: 'f'
+        format: "f",
       });
     }
   }
@@ -90,41 +95,38 @@ class App extends React.Component {
   handleFormSubmit(e) {
     e.preventDefault();
     //Validate input
-    fetch(getApiUrl(this.state.formInput), {mode: 'cors'})
-      .then(resp => {
-        if (resp.ok) {
-          const newCity = this.state.formInput
-          document.querySelector('.error').classList.remove('show-error')
-          this.setState({
-            city: newCity
-        })
-        } else {
-          document.querySelector('.error').classList.add('show-error')
-        }
-      })
+    fetch(getApiUrl(this.state.formInput), { mode: "cors" }).then((resp) => {
+      if (resp.ok) {
+        const newCity = this.state.formInput;
+        document.querySelector(".error").classList.remove("show-error");
+        this.setState({
+          city: newCity,
+        });
+      } else {
+        document.querySelector(".error").classList.add("show-error");
+      }
+    });
   }
 
   render() {
     return (
       <div>
         <h1>Weather App</h1>
-        <Image 
-          weather={this.state.weather}
-          gif={this.state.gif}/>
-        <Display 
+        <Image weather={this.state.weather} gif={this.state.gif} />
+        <Display
           onClick={this.toggleFormat.bind(this)}
           city={this.state.city}
           weather={this.state.weather}
           temp={this.state.temp}
           format={this.state.format}
         />
-        <CityForm 
+        <CityForm
           onChange={this.handleInput.bind(this)}
           onSubmit={this.handleFormSubmit.bind(this)}
         />
-        <span className='error'>City not found.</span>
+        <span className="error">City not found.</span>
       </div>
-    )
+    );
   }
 }
-  export default App;
+export default App;
--- src/CityForm.js
+++ src/CityForm.js
@@ -1,22 +1,17 @@
-import React from 'react';
+import React from "react";
 
 class CityForm extends React.Component {
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.props.onSubmit}>
-                    <label htmlFor="city-form">City:</label>
-                    <input 
-                        onChange={this.props.onChange}
-                        id="city-form">
-                    </input>
-                    <button type="submit">
-                        Get Weather
-                    </button>
-                </form>
-            </div>
-        )
-    } 
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.onSubmit}>
+          <label htmlFor="city-form">City:</label>
+          <input onChange={this.props.onChange} id="city-form"></input>
+          <button type="submit">Get Weather</button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default CityForm
+export default CityForm;
